2b8fb4f9a390abf9c04ebda71fca8d1bf81975c8
Nandini Bagga <bagganandini8@gmail.com>
2022-02-10 22:26:43 +0530

    basic structure of employee list in admin ui
    ascending, descending order of columns remaining

--- Admin UI/src/App.js
+++ Admin UI/src/App.js
@@ -1,11 +1,47 @@
 import './App.css';
-import VerticalNav from './components/VerticalNav';
+import { EmployeesTable } from './components/Employees/EmployeesTable';
+import { Footer } from './components/Footer/Footer';
+
+import {useState} from 'react';
 
 
 function App() {
+
+  // const onEdit = (employee_id, newRole) =>{
+  //   let newEditItem = employees.find((elem) =>{
+  //     return elem.employee_id === employee_id;
+  //   })
+  //   console.log(newEditItem, newRole);
+  //   //for deleting
+  //   // setEmployees(employees.filter((e)=>{
+  //   //   return e!==employee;
+  //   // }));
+  // }
+
+  const [employees, setEmployees] = useState([
+    {
+      employee_id: "ZOHO-1",
+      employee_name: "Janea",
+      employee_email: "jane@abc",
+      employee_role: "Platform user"
+    },
+    {
+      employee_id: "ZOHO-2",
+      employee_name: "Janeb",
+      employee_email: "jane@abc",
+      employee_role: "Platform Admin"
+    },
+    {
+      employee_id: "ZOHO-3",
+      employee_name: "Janec",
+      employee_email: "jane@abc",
+      employee_role: "Kaizen Committe"
+    }
+  ]);
   return (
     <div className='App'>
-      <VerticalNav/>
+      <EmployeesTable employees = {employees}/>
+      <Footer/>
     </div>
     
   );
